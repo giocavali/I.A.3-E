5945c57b26e272aab05bea2e612ad0b02df44379
giocavali <146956815+giocavali@users.noreply.github.com>
2024-08-08 07:44:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,10 +6,18 @@ const caixaresultado = document.querySelector('.caixa-resultado');
 const pergunta = [//abre a lista de objetos (itens)
     {//abre o item
         enunciado: "pergunta 1",
-        alternativa: [
-            "Alternativa 1"
-            "Alternativa 2"
-        ]
+        alternativa: ["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado: "pergunta 2",
+        alternativa: ["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado: "pergunta 3",
+        alternativa: ["Alternativa 1","Alternativa 2"]
     }
-
 ]
+let posicao = 0;
+let perguntaAtual;
+
+function mostraPergunta(){}
