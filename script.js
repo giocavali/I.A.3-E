19b196a501f3664a59f33cb2f7a88d516974631f
giocavali <146956815+giocavali@users.noreply.github.com>
2024-10-03 07:50:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,15 +6,40 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 const pergunta = [//abre a lista de objetos (itens)
     {//abre o item
         enunciado: "Você acha a imteligência artificial eficiente?",
-        alternativa: ["Sim","Não"]
+        alternativa: [{
+            texto:"Sim",
+            afirmação:"porque ela iniciou a nos ajudar no cotidiano."
+        
     },
     {
+        texto:"Não",
+        afirmação:"pois ela vem causando demência na inteligência humana, por conta da facilidade de se encontrar informações."
+    },
+    ]
+    {
         enunciado: "Você gosta da inteligência artificial?",
-        alternativa: ["Sim","Não"]
+        alternativa: [{
+            texto:"Sim",
+            afirmação:"porque ela é incrivivel."
+        
+    },
+    {
+        texto:"Não",
+        afirmação:"ela veio para controlar os seres humanos e vai nos manipular a favor do governo."
+    }
+    ]
     },
     {
         enunciado: "Você acha que a inteligência artificial vai acabar com o mundo e a humanidade?",
-        alternativa: ["Sim","Não"]
+        alternativa: [{
+            texto:"Sim",
+            afirmação:"com certeza, pois é uma coisa que o ser humano não pode controlar."
+    },
+    {
+        texto:"Não",
+        afirmação:"pois ela vai nos ajudar a crescer no universo."
+    }
+    ]
     }
 ]
 let posicao = 0;
@@ -28,7 +53,10 @@ function mostraPergunta(){
 function mostrarAlternativas(){
     for (const alternativa of perguntaAtual.alternativa){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativa.addEventListener("click", function(){
+            posicao++;
+        });
         caixaAlternativa.appendChild(botaoAlternativa);
     }
 }
