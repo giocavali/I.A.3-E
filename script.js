c57f7fa3276fbc1934cca54d100d8bce83b0745a
giocavali <146956815+giocavali@users.noreply.github.com>
2024-10-04 09:29:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,60 +3,74 @@ const caixaPergunta = document.querySelector('.caixa-pergunta');
 const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
 
-const pergunta = [//abre a lista de objetos (itens)
+const perguntas = [//abre a lista de objetos (itens)
     {//abre o item
+        enunciado: "Você gosta da Inteligência Artificial?",
+        alternativas: [{
+            texto: "Sim",
+            afirmação: "she's crazy."
+        },
+        {
+            texto: "Não",
+            afirmação: "ela veio para controlar os seres humanos e vai nos manipular a favor do governo."
+        }
+        ]
+    },
+    {
         enunciado: "Você acha a imteligência artificial eficiente?",
-        alternativa: [{
-            texto:"Sim",
-            afirmação:"porque ela iniciou a nos ajudar no cotidiano."
-        
-    },
-    {
-        texto:"Não",
-        afirmação:"pois ela vem causando demência na inteligência humana, por conta da facilidade de se encontrar informações."
-    },
-    ]
-    {
-        enunciado: "Você gosta da inteligência artificial?",
-        alternativa: [{
-            texto:"Sim",
-            afirmação:"porque ela é incrivivel."
-        
-    },
-    {
-        texto:"Não",
-        afirmação:"ela veio para controlar os seres humanos e vai nos manipular a favor do governo."
-    }
-    ]
+        alternativas: [{
+            texto: "Sim",
+            afirmação: "ela nos ajudar no cotidiano."
+        },
+        {
+            texto:"Não",
+            afirmação:"ela causa demência na inteligência humana, por conta da facilitação da busca de informações."
+        }
+        ]
     },
     {
         enunciado: "Você acha que a inteligência artificial vai acabar com o mundo e a humanidade?",
-        alternativa: [{
-            texto:"Sim",
-            afirmação:"com certeza, pois é uma coisa que o ser humano não pode controlar."
-    },
-    {
-        texto:"Não",
-        afirmação:"pois ela vai nos ajudar a crescer no universo."
-    }
-    ]
+        alternativas: [{
+            texto: "Sim",
+            afirmação: "ela vai nos MATAR!!"
+        },
+        {
+            texto: "Não",
+            afirmação: "claro que não"
+        }
+        ]
     }
 ]
 let posicao = 0;
 let perguntaAtual;
+let resposta = "";
 
-function mostraPergunta(){
-    perguntaAtual = perguntas[posicao].enunciado;
+
+function mostraPergunta() {
+    if (posicao>=perguntas.length){
+        mostraResultado();
+    return;
+    }
+    perguntaAtual = perguntas[posicao];
     caixaPergunta.textContent = perguntaAtual.enunciado;
+    caixaAlternativa.textContent = "";
     mostraAlternativas();
 }
-function mostrarAlternativas(){
-    for (const alternativa of perguntaAtual.alternativa){
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativa.addEventListener("click", function(){
-            posicao++;
-        });
-        caixaAlternativa.appendChild(botaoAlternativa);
+        botaoAlternativas.addEventListener("click",  ()=> respostasSelecionadas(alternativa));
+        caixaAlternativa.appendChild(botaoAlternativas);
     }
 }
+function respostasSelecionadas(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    respostas = afirmacoes;
+    posicao++;
+    mostraPergunta()
+}
+function mostraResultado(){
+    caixaPergunta.textContent = "Daqui a 10 anos... ";
+}
+mostraPergunta();
